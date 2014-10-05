82a12f43a5dc8bddcd186449469e7223a5b9ec28
Maxime Peterlin <rainbowlyte42@gmail.com>
2014-10-05 22:02:21 +0200

    client jalon01-final

--- src/jalon01/client.c
+++ src/jalon01/client.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <errno.h>
 
+#include <arpa/inet.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
@@ -15,6 +16,8 @@ int do_socket();
 void do_connect(int sock, struct sockaddr_in *serv_info);
 void readline(char* buf);
 void handle_client_message(int sock, char* buffer);
+ssize_t readline(int fd, void *str, size_t maxlen);
+ssize_t sendline(int fd, const void *str, size_t maxlen);
 
 int main(int argc,char** argv)
 {
@@ -47,17 +50,25 @@ int main(int argc,char** argv)
 /* get_addr_info fills the sockaddr_in structure with information such as the server's ip address, the server's port, etc... */
 int get_addr_info(struct sockaddr_in* serv_info, char* host, char* port)
 {
-	struct hostent* h = gethostbyname(host);
+	//struct hostent* h = gethostbyname(host);
+	struct addrinfo *res, hint;
+	struct sockaddr_in *addr;
+
+	memset(&hint, 0, sizeof(struct addrinfo));
+
+	/* getaddrinfo returns 0 on success */
+	if( getaddrinfo(host, port, &hint, &res) )
+    {
+    	perror("getaddrinfo");
+    	exit(errno);
+    }
 
     memset(serv_info, 0, sizeof(*serv_info));
     	serv_info->sin_family = AF_INET;
     	serv_info->sin_port =  htons(atoi(port));
 
-    if (inet_aton(host, serv_info->sin_addr) != 1)
-    {
-    	perror("inet_aton");
-    	exit(errno);
-    }
+    addr = (struct sockaddr_in *)res->ai_addr; 
+    serv_info->sin_addr = addr->sin_addr;
 
     return 0;
 }
@@ -86,24 +97,50 @@ void do_connect(int sock, struct sockaddr_in *serv_info)
 	}
 }
 
-/* readline stores the user input into buffer buf while protecting the application from a buffer overflow */
-void readline(char* buf)
-{
-	char f[20]; 
-
-	memset(buf, 0, STRLEN);
-	printf("> ");
-	fgets(buf, STRLEN, stdin);
-}
-
-/* handle_client_message sends the message contained into the buffer buf to the server */
+/* handle_client_message stores the user input into buffer buf while protecting the application from a buffer overflow
+   and sends the message contained into the buffer buf to the server */
 void handle_client_message(int sock, char* buffer)
 {
 	int n = 0;
+	char f[20]; 
 
-	if( write(sock, buffer, strlen(buffer)) < 0 )
+	memset(buffer, 0, STRLEN);
+	printf("> ");
+	fgets(buffer, STRLEN, stdin);
+
+	sendline(sock, buffer, strlen(buffer));
+	readline(sock, buffer, strlen(buffer));
+
+	printf("Server ECHO : %s\n", buffer);
+}
+
+ssize_t readline(int sock, void *buffer, size_t maxlen)
+{
+	ssize_t n = read(sock, buffer, maxlen);
+	
+	if( n < 0 )
+	{
+		perror("Read");
+		exit(errno);
+	}
+	else if( n == 0 )
+	{
+		perror("Server dead");
+		close(sock);
+		exit(errno);
+	}
+
+	return n;
+}
+
+ssize_t sendline(int fd, const void *buffer, size_t maxlen)
+{
+	ssize_t n;
+	if( (n = write(sock, buffer, maxlen)) < 0 )
 	{
 		perror("Write");
 		exit(errno);
 	}
+
+	return n;
 }
